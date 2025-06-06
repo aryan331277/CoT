267d6a83bbc525549f2638ada07e5f8ea29709a8
aryan <bt24csd009@iiitn.ac.in>
2025-06-06 14:05:33 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,10 +1,12 @@
-from utils import call_model
+# agent.py
+
+from hf_utils import call_hf_model as call_model
 from prompts import THINKER_PROMPT, CRITIC_PROMPT
 
 def thinker(question):
     prompt = THINKER_PROMPT.format(question=question)
-    return call_model("mixtral", prompt)
+    return call_model(prompt)
 
 def critic(reasoning):
     prompt = CRITIC_PROMPT.format(reasoning=reasoning)
-    return call_model("mistral", prompt)
+    return call_model(prompt)
