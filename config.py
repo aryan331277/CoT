717ca4fd2d57e715ac731ebd41be38771458d3dd
aryan <bt24csd009@iiitn.ac.in>
2025-06-06 22:06:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"  # 👈 replace with any from above
+MODEL_NAME = "tiiuae/falcon-7b-instruct"  # 👈 replace with any from above
 HF_API_URL = f"https://api-inference.huggingface.co/models/{MODEL_NAME}"
