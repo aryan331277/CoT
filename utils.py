84a8a8994e094c23ba15ce7e8d8dcfb40973e88b
aryan <bt24csd009@iiitn.ac.in>
2025-06-06 11:08:33 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+from ollama import Client
+
+client = Client()
+
+def call_model(model_name, prompt):
+    response = client.generate(model=model_name, prompt=prompt)
+    return response['response']
